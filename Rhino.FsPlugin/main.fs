66f4d2b827aa082dd297f0eb11a0bbe6a64c166d
Goswin <gwinsky@yahoo.com>
2016-06-16 15:44:59 +0200

    Add project files.

---
+++ Rhino.FsPlugin/main.fs
@@ -0,0 +1,43 @@
+﻿namespace FsPlugin
+
+
+
+//open Rhino
+
+// the Plugin:
+type FsPlugin private () =  // with private constructor for singelton   
+    inherit Rhino.PlugIns.PlugIn()
+    
+    // Every RhinoCommon .rhp assembly must have one and only one PlugIn-derived
+    // class. DO NOT create instances of this class yourself. It is the
+    // responsibility of Rhino to create an instance of this class.
+    // <para>To complete plug-in information, please also see all PlugInDescription
+    // attributes in AssemblyInfo.cs (you might need to click 
+    // "Project" -> "Show All Files" to see it in the "Solution Explorer" window).
+    
+    // You can override methods here to change the plug-in behavior on
+    // loading and shut down, add options pages to the Rhino _Option command
+    // and mantain plug-in wide options in a document .
+    // Singelton: http://stackoverflow.com/questions/2691565/how-to-implement-singleton-pattern-syntax
+    static member val Instance = FsPlugin()
+     
+    
+
+// the Command(s):
+[<System.Runtime.InteropServices.Guid("ce66e7f8-7f27-476d-a30e-1540a04c25eb")>]
+type FsPluginCommand () = // calls for the command that starts sthe script editor
+    inherit Rhino.Commands.Command()    
+    static member val Instance = FsPluginCommand() // Singelton: http://stackoverflow.com/questions/2691565/how-to-implement-singleton-pattern-syntax
+    // Rhino only creates one instance of each command class defined in a
+    // plug-in, so it is safe to store a refence in a static property.
+                    
+    override this.EnglishName = "xxxxx" //The command name as it appears on the Rhino command line.
+           
+    override this.RunCommand (doc, mode)  =        
+        // perform action...
+        //...
+
+        Rhino.Commands.Result.Success
+
+
+
